d6273c48a9ae1c350732cd58b065cba26ab882f6
dmitrii-kulganek <dmitrii.kulganek@ognivp.ru>
2023-03-04 17:43:22 +0300

    гененация массива объектов. добавление модульности

---
+++ js/util.js
@@ -0,0 +1,25 @@
+//Функция генерации уникальных идентификаторов
+function createIdGenerator (start) {
+  let lastGeneratedId = start;
+
+  return function () {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+}
+
+// Функция генерации для получения уникальных целых чисел из указанного диапазона
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+// Функция для получения случайного элемента массива, которая
+// принимает в качестве параметра один массив, а
+// возвращает его случайное значение.
+const arrayRandElement = (array) => array[getRandomInteger(0, array.length - 1)];
+
+export {createIdGenerator, getRandomInteger, arrayRandElement};
