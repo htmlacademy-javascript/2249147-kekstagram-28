cd28965cedc76f222d8c3d454abad4e747f3c879
dmitrii-kulganek <dmitrii.kulganek@ognivp.ru>
2023-04-14 11:59:50 +0300

    исправление незачтённых критериев. исправление адреса отправки данных

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 const BASE_URL = 'https://28.javascript.pages.academy/kekstagram';
 const Route = {
   GET_DATA: '/data',
-  SEND_DATA: '/123',
+  SEND_DATA: '/',
 };
 const Method = {
   GET: 'GET',
